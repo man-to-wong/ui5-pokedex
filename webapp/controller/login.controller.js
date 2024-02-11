1f0e4224f3fe4b96981f9bae2bb5ba8d2390556f
man-to-wong <man-to.wong@hotmail.de>
2024-02-12 00:13:33 +0100

    Credentials JSON-Object implementiert

--- webapp/controller/login.controller.js
+++ webapp/controller/login.controller.js
@@ -1,17 +1,19 @@
 sap.ui.define(['sap/ui/core/mvc/Controller',
                 'sap/m/MessageToast',
             'sap/ui/model/json/JSONModel'],
-	function(Controller, MessageToast) {
+	function(Controller, MessageToast,JSONModel) {
 	"use strict";
-    var user= 'DEMOUSER';
-    var pw = '0000';
+
 	var CController = Controller.extend("pokedex.demo.controller.login", {
         onPress: function (oEvent){
             var oView = this.getView();
             var inputUsername= oView.byId('userInput').getValue();
             var inputPassword = oView.byId('pwInput').getValue();
+            var credentials = this.getOwnerComponent().getModel("credentials").getData();
+            var user = credentials.Username;
+            var pw = credentials.Password;
             var oRouter = this.getOwnerComponent().getRouter();
-
+            
             if(user === inputUsername && pw === inputPassword){
                 MessageToast.show('Erfolgreicher Login!');
                 oRouter.navTo("pokedex",null);
